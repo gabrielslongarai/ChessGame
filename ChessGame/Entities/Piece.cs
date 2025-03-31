acff5c9c38232547a53b80eac59f1eb79a6fb9e6
Gabriel Longarai <gabrielslongarai@gmail.com>
2025-03-31 17:41:19 -0300

    Add Board, Piece, and Color classes for chess game
    Introduces three new classes: `Board`, `Piece`, and `Color` in the `ChessGame.Entities` and `ChessGame.Entities.Enums` namespaces. The `Board` class is currently empty, while the `Piece` class includes properties for `Position`, `Color`, `MoveCount`, and a reference to the `Board`. The `Color` enum defines two values: `White` and `Black`.

---
+++ ChessGame/Entities/Piece.cs
@@ -0,0 +1,20 @@
+﻿using ChessGame.Entities.Enums;
+using ChessGame.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessGame.Entities
+{
+    internal class Piece
+    {
+        public Position Position { get; set; }
+        public Color Color { get; protected set; }
+        public int MoveCount { get; protected set; }
+        public Board Board { get; protected set; }
+
+
+    }
+}
