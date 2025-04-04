11b2934b59319f62befda475e8ca16be45ccdef7
Gabriel Longarai <gabrielslongarai@gmail.com>
2025-04-04 19:59:48 -0300

    Implement PossibleMoves method for Rook class
    Updated the `Rook` class in the `ChessGame.Roles` namespace to provide a complete implementation of the `PossibleMoves` method. The new method calculates all possible moves for the Rook piece, checking all four directions (up, down, left, right) and marking valid positions in a boolean array while considering the presence and color of other pieces.

--- ChessGame/Roles/Rook.cs
+++ ChessGame/Roles/Rook.cs
@@ -7,7 +7,59 @@ namespace ChessGame.Roles
     {
         public override bool[,] PossibleMoves()
         {
-            throw new NotImplementedException();
+            bool[,] possibleMoves = new bool[Board.Lines, Board.Columns];
+
+            Position position = new Position(0, 0);
+
+            // Up
+            position.SetValues(position.Line - 1, position.Column);
+            while (Board.IsValidPosition(position) && CanMove(position))
+            {
+                possibleMoves[position.Line, position.Column] = true;
+                if (Board.HasPiece(position) && Board.GetPiece(position).Color != this.Color)
+                {
+                    break;
+                }
+                position.Line--;
+            }
+
+            // Down
+            position.SetValues(position.Line + 1, position.Column);
+            while (Board.IsValidPosition(position) && CanMove(position))
+            {
+                possibleMoves[position.Line, position.Column] = true;
+                if (Board.HasPiece(position) && Board.GetPiece(position).Color != this.Color)
+                {
+                    break;
+                }
+                position.Line++;
+            }
+
+            // Left
+            position.SetValues(position.Line, position.Column - 1);
+            while (Board.IsValidPosition(position) && CanMove(position))
+            {
+                possibleMoves[position.Line, position.Column] = true;
+                if (Board.HasPiece(position) && Board.GetPiece(position).Color != this.Color)
+                {
+                    break;
+                }
+                position.Column--;
+            }
+
+            // Right
+            position.SetValues(position.Line, position.Column + 1);
+            while (Board.IsValidPosition(position) && CanMove(position))
+            {
+                possibleMoves[position.Line, position.Column] = true;
+                if (Board.HasPiece(position) && Board.GetPiece(position).Color != this.Color)
+                {
+                    break;
+                }
+                position.Column++;
+            }
+
+            return possibleMoves;
         }
 
         public override string ToString()
