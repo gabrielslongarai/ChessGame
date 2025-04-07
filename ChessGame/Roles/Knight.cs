ce9645e75c2b854e01beb0af50639e894adb777b
Gabriel Longarai <gabrielslongarai@gmail.com>
2025-04-07 11:30:30 -0300

    Refactor ChessMatch initialization and implement Knight moves
    Removed commented-out pawn loop in ChessMatch.cs and directly added pieces to specific positions.
    
    Implemented the PossibleMoves method in Knight.cs to calculate valid moves based on the Knight's movement pattern.

--- ChessGame/Roles/Knight.cs
+++ ChessGame/Roles/Knight.cs
@@ -12,9 +12,66 @@ namespace ChessGame.Roles
 
         public override bool[,] PossibleMoves()
         {
-            throw new NotImplementedException();
+            bool[,] possibleMoves = new bool[Board.Lines, Board.Columns];
+            Position position = new Position(0, 0);
+
+            // Up 2, Right 1
+            position.SetValues(Position.Line - 2, Position.Column + 1);
+            if (Board.IsValidPosition(position) && CanMove(position))
+            {
+                possibleMoves[position.Line, position.Column] = true;
+            }
+
+            // Up 2, Left 1
+            position.SetValues(Position.Line - 2, Position.Column - 1);
+            if (Board.IsValidPosition(position) && CanMove(position))
+            {
+                possibleMoves[position.Line, position.Column] = true;
+            }
+
+            // Down 2, Right 1
+            position.SetValues(Position.Line + 2, Position.Column + 1);
+            if (Board.IsValidPosition(position) && CanMove(position))
+            {
+                possibleMoves[position.Line, position.Column] = true;
+            }
+
+            // Down 2, Left 1
+            position.SetValues(Position.Line + 2, Position.Column - 1);
+            if (Board.IsValidPosition(position) && CanMove(position))
+            {
+                possibleMoves[position.Line, position.Column] = true;
+            }
+
+            // Right 2, Up 1
+            position.SetValues(Position.Line - 1, Position.Column + 2);
+            if (Board.IsValidPosition(position) && CanMove(position))
+            {
+                possibleMoves[position.Line, position.Column] = true;
+            }
+
+            // Right 2, Down 1
+            position.SetValues(Position.Line + 1, Position.Column + 2);
+            if (Board.IsValidPosition(position) && CanMove(position))
+            {
+                possibleMoves[position.Line, position.Column] = true;
+            }
+
+            // Left 2, Up 1
+            position.SetValues(Position.Line - 1, Position.Column - 2);
+            if (Board.IsValidPosition(position) && CanMove(position))
+            {
+                possibleMoves[position.Line, position.Column] = true;
+            }
+
+            // Left 2, Down 1
+            position.SetValues(Position.Line + 1, Position.Column - 2);
+            if (Board.IsValidPosition(position) && CanMove(position))
+            {
+                possibleMoves[position.Line, position.Column] = true;
+            }
+
+            return possibleMoves;
         }
-
-
     }
 }
