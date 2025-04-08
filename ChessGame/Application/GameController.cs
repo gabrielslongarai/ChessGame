23c5141f9c599016482586f265a55c5ae147548b
Gabriel Longarai <gabrielslongarai@gmail.com>
2025-04-07 23:10:24 -0300

    Remove unused using directives in GameController.cs
    Eliminated several unnecessary `using` directives for
    namespaces such as `System`, `System.Collections.Generic`,
    `System.Linq`, `System.Text`, and `System.Threading.Tasks`
    from the `GameController.cs` file to clean up the code
    and reduce dependencies.

--- ChessGame/Application/GameController.cs
+++ ChessGame/Application/GameController.cs
@@ -1,11 +1,6 @@
 ﻿using ChessGame.Board;
 using ChessGame.Exceptions;
 using ChessGame.Roles;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ChessGame.Application
 {
